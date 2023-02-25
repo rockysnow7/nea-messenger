557f82f1086c07f37799d51c076ad5e06869799b
Finn Eyles <rockysnow7@gmail.com>
2023-02-25 00:17:49 +0000

    tracking

--- prototype/README.md
+++
@@ -1,6 +0,0 @@
-this package handles database functionality, including:
-- creating/saving users
-- creating/saving chats
-- creating/saving messages
-
-this is also my prototype <3
--- prototype/ip.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-def get_host_ip_addr() -> str:
-    """
-    Returns the IP address of the host machine.
-    """
-
-    return socket.gethostbyname_ex(socket.gethostname())[-1][-1]
